a74ee180730668db45f59673aebea74fdc00a606
teddy <teddy@podotree.com>
2017-06-23 22:35:50 +0900

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+#FROM python:3.5
+#
+#COPY . /usr/src/app
+#RUN pip install --no-cache-dir -r /usr/src/app/requirements.txt
+#
+#WORKDIR /usr/src/app
+
+FROM python:3.5-onbuild
--- scripts/test.py
+++ scripts/test.py
@@ -1,8 +1,9 @@
-
-
-
+from db.models import User
 
 
 def run(*script_args):
 
-	print('hello world')
+	user_list = User.objects.all()
+
+	for user in user_list:
+		print('name : ', user.name)
